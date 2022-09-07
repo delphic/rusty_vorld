edb7b3ea37b0e49aa09b03e36dd05d15744cd809
delphic <delphic.uk@gmail.com>
2022-09-07 11:19:07 +0100

    Converted camer movement shape casts to distance based Added skin_depth and second slide calculation

--- src/main.rs
+++ src/main.rs
@@ -85,49 +85,89 @@ fn move_camera(
     let local_z = camera_transform.local_z();
 
     let velocity = movement_speed * delta_x * local_x + movement_speed * delta_z * local_z;
-    let time_delta = time.delta_seconds();
 
-    let shape = Collider::ball(0.25);
-    let start_translation = camera_transform.translation;
+    if velocity.length_squared() > 0.0 {
+        let time_delta = time.delta_seconds();
 
-    if let Some((_, hit)) = rapier_context.cast_shape(
-        camera_transform.translation,
-        Quat::IDENTITY,
-        velocity,
-        &shape,
-        time_delta + 0.001,
-        QueryFilter::default()
-    ) {
-        if hit.toi == 0.0 {
-            panic!("Started camera movement already overlapping");
-        } else {
-            let stop_time = f32::max(hit.toi - 0.001, 0.0); // adjusted to allow for a minimum separation - TODO: should do this by distance not time
-            camera_transform.translation += velocity * stop_time;
-            
-            let time_remainder = time_delta - stop_time;
-            let velocity_remainder = velocity * time_remainder / time_delta;
-            let slide_velocity = velocity_remainder - velocity_remainder.dot(hit.normal1) * hit.normal1;
-            if rapier_context.cast_shape(
-                camera_transform.translation,
-                Quat::IDENTITY,
-                slide_velocity,
-                &shape,
-                time_remainder + 0.001,
-                QueryFilter::default()).is_none() {
+        let collider_radius = 0.25;
+        let shape = Collider::ball(collider_radius);
+        let start_translation = camera_transform.translation;
+    
+        let skin_depth = 0.01;
+        let velocity_direction = velocity.normalize();
+        let velocity_magnitude = velocity.length();
+
+        if let Some((_, hit)) = rapier_context.cast_shape(
+            camera_transform.translation,
+            Quat::IDENTITY,
+            velocity_direction,
+            &shape,
+            time_delta * velocity_magnitude + skin_depth,
+            QueryFilter::default()
+        ) {
+            // desired movement collides, attempt to slide along surface
+            if hit.toi == 0.0 {
+                panic!("Started camera movement already overlapping");
+            } else {
+                // NOTE: Casting in velocity direction means time of impact is in fact distance to impact
+                let close_distance = hit.toi - skin_depth;
+                camera_transform.translation += velocity_direction * close_distance;
+                // ^^ This can be negative and will attempt to move the camera away before sliding along the surface
+
+                let stop_time = close_distance / velocity_magnitude;
+                let time_remainder = time_delta - stop_time;
+                let velocity_remainder = velocity * time_remainder / time_delta;
+                let slide_velocity = velocity_remainder - velocity_remainder.dot(hit.normal1) * hit.normal1;
+                let slide_velocity_direction = slide_velocity.normalize();
+                let slide_velocity_magnitude = slide_velocity.length();
+                
+                if let Some((_, second_hit)) = rapier_context.cast_shape(
+                    camera_transform.translation,
+                    Quat::IDENTITY,
+                    slide_velocity_direction,
+                    &shape,
+                    time_remainder * slide_velocity_magnitude + skin_depth,
+                    QueryFilter::default()
+                ) {
+                    // slide also collides, attempt to one further slide in direction perpenticular to both hit normals
+                    let second_slide_direction = hit.normal1.cross(second_hit.normal1);
+
+                    let close_distance = second_hit.toi - skin_depth;
+                    camera_transform.translation += slide_velocity_direction * close_distance;
+                    
+                    let time_delta = time_remainder;
+                    let stop_time = close_distance / slide_velocity_magnitude;
+                    let time_remainder = time_delta - stop_time;
+                    let velocity_remainder = slide_velocity * time_remainder / time_delta;
+                    let slide_velocity = velocity_remainder.dot(second_slide_direction) * second_slide_direction;
+                    
+                    if rapier_context.cast_shape(
+                        camera_transform.translation,
+                        Quat::IDENTITY,
+                        slide_velocity.normalize(),
+                        &shape,
+                        time_remainder * slide_velocity.length()  + skin_depth,
+                        QueryFilter::default()
+                    ).is_none() {
+                        // Only move if there are no collisions only second slide axis, else only move up to second contact
+                        camera_transform.translation += slide_velocity * time_remainder;
+                    }
+                } else {
                     camera_transform.translation += slide_velocity * time_remainder;
-            } // TODO: Attempt move up to new contact and second slide axis before giving up
+                }
+            }
+        } else {
+            camera_transform.translation += velocity * time_delta;
         }
-    } else if velocity.length_squared() > 0.0 {
-        camera_transform.translation += velocity * time_delta;
-    }
 
-    rapier_context.intersections_with_shape(camera_transform.translation, Quat::IDENTITY, &shape, QueryFilter::default(), |_| {
-        // Cast Shape sometimes lies about there being no collision due to float precision issues,
-        // so check for intersections and if found restore to starting position
-        debug::log_warning("Camera shape found to intersect world collider after movement, restoring to last valid position");
-        camera_transform.translation = start_translation;
-        false
-    });
+        rapier_context.intersections_with_shape(camera_transform.translation, Quat::IDENTITY, &shape, QueryFilter::default(), |_| {
+            // cast_shape sometimes lies about there being no collision due to float precision issues,
+            // so check for intersections and if found restore to starting position
+            debug::log_warning("Camera shape found to intersect world collider after movement, restoring to last valid position");
+            camera_transform.translation = start_translation;
+            false
+        });
+    }
 }
 
 fn clamp(value: f32, min: f32, max: f32) -> f32 {
