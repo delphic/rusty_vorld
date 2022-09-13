2648eed085cac94c1b1e67e4928644a33fc7aa99
delphic <delphic.uk@gmail.com>
2022-09-13 11:23:59 +0100

    Added world structure for multiple chunks

--- src/voxel.rs
+++ src/voxel.rs
@@ -3,6 +3,7 @@ use super::mesher;
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 use std::collections::HashMap;
+use std::convert::TryInto;
 
 #[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
 pub enum Direction {
@@ -20,7 +21,63 @@ pub struct VoxelConfig {
 }
 
 pub const CHUNK_SIZE: usize = 16;
+pub const CHUNK_SIZE_I32: i32 = 16;
+pub const CHUNK_SIZE_F32: f32 = 16.0;
 pub const CHUNK_ARRAY_SIZE: usize = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
+
+pub struct Vorld {
+    pub chunks: HashMap<IVec3, Chunk>,
+}
+
+impl Vorld {
+    /// gets chunk index for a voxel at index v in world space on a given axis
+    fn get_chunk_index(v: i32) -> i32 {
+        if v >= 0 || v % CHUNK_SIZE_I32 == 0 {
+            v / CHUNK_SIZE_I32
+        } else {
+            (v / CHUNK_SIZE_I32) - 1
+        }
+    }
+
+    fn get_chunk_key(x: i32, y: i32, z: i32) -> IVec3 {
+        IVec3::new(Self::get_chunk_index(x), Self::get_chunk_index(y), Self::get_chunk_index(z))
+    }
+
+    fn add_voxel(&mut self, id: u8, x: i32, y: i32, z: i32) {
+        let key = Self::get_chunk_key(x, y, z);
+        if let Some(chunk) = self.chunks.get_mut(&key) {
+
+            let i = (x - key.x * CHUNK_SIZE_I32).try_into().unwrap();
+            let j = (y - key.y * CHUNK_SIZE_I32).try_into().unwrap();
+            let k = (z - key.z * CHUNK_SIZE_I32).try_into().unwrap();
+            chunk.add_voxel(id, i, j, k);
+        } else {
+            let mut chunk = Chunk { voxels: [ BlockIds::Air as u8; CHUNK_ARRAY_SIZE ]};
+
+            let i = (x - key.x * CHUNK_SIZE_I32).try_into().unwrap();
+            let j = (y - key.y * CHUNK_SIZE_I32).try_into().unwrap();
+            let k = (z - key.z * CHUNK_SIZE_I32).try_into().unwrap();
+            chunk.add_voxel(id, i, j, k);
+            self.chunks.insert(key, chunk);
+        }
+    }
+}
+
+#[test]
+fn test_key() {
+    assert_eq!(Vorld::get_chunk_index(-15), -1);
+    assert_eq!(Vorld::get_chunk_index(-16), -1);
+}
+
+fn point_in_chunk(v: i32) -> i32 {
+    if v % CHUNK_SIZE_I32 == 0 {
+        0
+    } else if v >= 0 {
+        v % CHUNK_SIZE_I32
+    } else {
+        v % CHUNK_SIZE_I32 + CHUNK_SIZE_I32
+    }
+}
 
 pub struct Chunk {
     pub voxels: [u8; CHUNK_ARRAY_SIZE],
@@ -81,40 +138,46 @@ pub fn setup(
     voxel_config: Res<VoxelConfig>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    let mut chunk = Chunk {
-        voxels: [0; CHUNK_ARRAY_SIZE],
+    let mut vorld = Vorld {
+        chunks: HashMap::new(),
     };
-    for x in 0..CHUNK_SIZE {
-        for z in 0..CHUNK_SIZE {
-            chunk.add_voxel(BlockIds::Grass as u8, x, 0, z);
+
+    for x in -16..32 {
+        for z in -16..32 {
+            vorld.add_voxel(BlockIds::Grass as u8, x, 0, z);
+            if (point_in_chunk(x) == 0 || point_in_chunk(x) == 15) && (point_in_chunk(z) == 0 || point_in_chunk(z) == 15) {
+                vorld.add_voxel(BlockIds::StoneBlocks as u8, x, 1, z);
+            }
         }
     }
 
     for x in 4..=12 {
         for z in 4..=12 {
-            for y in 1..4 {
+            for y in 1..18 {
                 if (x == 4 || x == 12 || z == 4 || z == 12) && !((x == 8 || x == 9) && z == 4 && y <= 2) {
-                    chunk.add_voxel(BlockIds::StoneBlocks as u8, x, y, z);
+                    vorld.add_voxel(BlockIds::StoneBlocks as u8, x, y, z);
                 }
             }
-            chunk.add_voxel(BlockIds::StoneBlocks as u8, x, 4, z);
+            vorld.add_voxel(BlockIds::StoneBlocks as u8, x, 18, z);
         }
     }
 
-    let mut tile_meshes = mesher::build_chunk_meshes(&chunk, &voxel_config);
+    for (key, chunk) in vorld.chunks.iter() {
+        let mut tile_meshes = mesher::build_chunk_meshes(&chunk, &voxel_config);
 
-    while let Some((tile_id, mesh)) = tile_meshes.pop() {
-        let mut entity_commands = commands.spawn();
-        if let Some(collider) = Collider::from_bevy_mesh(&mesh, &ComputedColliderShape::TriMesh) {
-            entity_commands.insert(collider);
-        } else {
-            error!("Unable to generate mesh collider");
+        while let Some((tile_id, mesh)) = tile_meshes.pop() {
+            let mut entity_commands = commands.spawn();
+            if let Some(collider) = Collider::from_bevy_mesh(&mesh, &ComputedColliderShape::TriMesh) {
+                entity_commands.insert(collider);
+            } else {
+                error!("Unable to generate mesh collider");
+            }
+            entity_commands.insert_bundle(MaterialMeshBundle {
+                mesh: meshes.add(mesh),
+                material: atlas.materials[&tile_id].clone(),
+                transform: Transform::from_xyz(key.x as f32 * CHUNK_SIZE_F32, key.y as f32 * CHUNK_SIZE_F32, key.z as f32 * CHUNK_SIZE_F32),
+                ..default()
+            });
         }
-        entity_commands.insert_bundle(MaterialMeshBundle {
-            mesh: meshes.add(mesh),
-            material: atlas.materials[&tile_id].clone(),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            ..default()
-        });
     }
 }
